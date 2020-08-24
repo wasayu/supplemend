fd0c4a1170e3003a6c69f1ce78c69bea7baca827
yuki.iwasa <yuuki10022828@gmail.com>
2020-08-24 14:30:55 +0000

    seed

--- db/seed/suggest_detail.rb
+++ db/seed/suggest_detail.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.foreach('db/csv/suggest_detail.csv') do |row|
+CSV.foreach('db/csv/suggest_detail_seed.csv') do |row|
   SuggestDetail.create(
     :supplement_id => row[0],
     :suggestion_id => row[1]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-SuggestDetail.create(supplement_id: 379, suggestion_id: 12)
+require 'csv'
+
+CSV.foreach('db/csv/suppl_tag_seed.csv') do |row|
+  SupplTag.create(
+    :supplement_id => row[0],
+    :tag_id => row[1],
+    :primary_tag => row[2]
+  )
+end
