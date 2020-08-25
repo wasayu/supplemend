e6376c1f8e295b2c803597abce4750c9c1c3a6f0
yuki.iwasa <yuuki10022828@gmail.com>
2020-08-25 13:19:20 +0000

    heroku_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 require 'csv'
 
-CSV.foreach('db/csv/suppl_tag_seed.csv') do |row|
-  SupplTag.create(
-    :supplement_id => row[0],
-    :tag_id => row[1],
-    :primary_tag => row[2]
+CSV.foreach('db/csv/supplementsテーブル.csv') do |row|
+  Supplement.create(
+    :item_name => row[0],
+    :brand => row[1],
+    :suppl_name => row[2],
+    :made => row[3],
+    :flavor => row[4],
+    :price => row[5],
+    :price_tag => row[6],
+    :use_time => row[7],
+    :calorie => row[8],
+    :lipid => row[9],
+    :carbo => row[10],
+    :protein => row[11],
+    :img => row[12],
+    :url => row[13]
   )
 end
