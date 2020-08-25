86609c1b323d7af9d387a40c9a00cef3a7841a1a
yuki.iwasa <yuuki10022828@gmail.com>
2020-08-25 14:02:43 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,7 @@ CSV.foreach('db/csv/suggestion_seed.csv') do |row|
   Suggestion.create(
     :content => row[0],
     :purpose => row[1],
-    :budget => row[2],
-    :protein_flavor => row[3],
-    :amino_flavor => row[4]
+    :budget => row[2]
   )
 end
 
